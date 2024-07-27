77ba416eaf63e48e41add8fafe3cafefebdfbf25
Omar Zapata <andresete.chaos@gmail.com>
2024-07-27 11:03:16 -0500

    [kahi_scienti_sample] added error control

--- Kahi_scienti_sample/kahi_scienti_sample/Kahi_scienti_sample.py
+++ Kahi_scienti_sample/kahi_scienti_sample/Kahi_scienti_sample.py
@@ -37,9 +37,9 @@ class Kahi_scienti_sample(KahiBase):
             db_in = client_in[db["database_name"]]
             collection_in = db_in[db["collection_name"]]
 
-            self.dbs_in.append({"client": client_in, "db": db_in, "collection": collection_in})
+            self.dbs_in.append(
+                {"client": client_in, "db": db_in, "collection": collection_in})
         self.verbose = self.config["scienti_sample"]["verbose"] if "verbose" in self.config["scienti_sample"] else 1
-
 
     def process_products(self):
         """
@@ -48,6 +48,11 @@ class Kahi_scienti_sample(KahiBase):
         if "products" in self.config["scienti_sample"] and self.config["scienti_sample"]["products"]:
             product_ids = []
             for product in self.config["scienti_sample"]["products"]:
+                if "COD_RH" not in product and "COD_PRODUCTO" not in product:
+                    print(
+                        "ERROR: COD_RH and COD_PRODUCTO are required in the product configuration")
+                    raise Exception(
+                        "COD_RH and COD_PRODUCTO are required in the product configuration")
                 product_ids.append(dict(product))
             if self.verbose > 0:
                 print("INFO: Processing products: ", len(product_ids))
@@ -59,10 +64,13 @@ class Kahi_scienti_sample(KahiBase):
                             self.collection.insert_one(work)
                         else:
                             if self.verbose > 2:
-                                print(f"INFO: Product {product_id} already exists in db {self.db.name} collection {self.collection.name}")
+                                print(
+                                    f"INFO: Product {product_id} already exists in db {self.db.name} collection {self.collection.name}")
                     else:
                         if self.verbose > 2:
-                            print(f"WARNING: Product {product_id} not found in the database {db['db'].name} collection {db["collection"].name}")
+                            print(
+                                f"WARNING: Product {product_id} not found in the database {db['db'].name} collection {db["collection"].name}")
+
     def process_authors(self):
         """
         Process authors given the COD_RH in the workflow configuration.
@@ -75,20 +83,25 @@ class Kahi_scienti_sample(KahiBase):
             for author_id in author_ids:
                 for db in self.dbs_in:
                     if self.verbose > 0:
-                        print(f"INFO: Found {db['collection'].count_documents({'COD_RH':author_id})} in db {db['db'].name} collection {db['collection'].name} for id   {author_id}")
-                    works = db["collection"].find({"COD_RH":author_id})
-                    Parallel(n_jobs=self.num_jobs,backend="threading",verbose=10)(delayed(self.process_one_work)(work) for work in works)
-    def process_one_work(self,work):
+                        print(
+                            f"INFO: Found {db['collection'].count_documents({'COD_RH': author_id})} in db {db['db'].name} collection {db['collection'].name} for id   {author_id}")
+                    works = db["collection"].find({"COD_RH": author_id})
+                    Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
+                        delayed(self.process_one_work)(work) for work in works)
+
+    def process_one_work(self, work):
         """
         Method to process one work and save it in the output database.
         Required for parallel processing.
         """
-        work_id = {"COD_RH":work["COD_RH"], "COD_PRODUCTO":work["COD_PRODUCTO"]}
+        work_id = {"COD_RH": work["COD_RH"],
+                   "COD_PRODUCTO": work["COD_PRODUCTO"]}
         if self.collection.count_documents(work_id) == 0:
             self.collection.insert_one(work)
         else:
             if self.verbose > 2:
-                print(f"INFO: Work  {work_id} already exists in db {self.db.name} collection {self.collection.name} ")
+                print(
+                    f"INFO: Work  {work_id} already exists in db {self.db.name} collection {self.collection.name} ")
 
     def process_types(self):
         """
@@ -100,24 +113,33 @@ class Kahi_scienti_sample(KahiBase):
         if "types" in self.config["scienti_sample"] and self.config["scienti_sample"]["types"]:
             type_ids = []
             for type_id in self.config["scienti_sample"]["types"]:
+                if "COD_TIPO_PRODUCTO" not in type_id:
+                    print(
+                        "ERROR: COD_TIPO_PRODUCTO is required in the type configuration")
+                    raise Exception(
+                        "COD_TIPO_PRODUCTO is required in the type configuration")
                 type_ids.append(dict(type_id))
             if self.verbose > 0:
                 print("INFO: Processing types: ", len(type_ids))
             for type_id in type_ids:
                 for db in self.dbs_in:
                     if self.verbose > 0:
-                        count = db["collection"].count_documents({"$or":[{"product_type":{"$elemMatch":type_id}},
-                                                          {"product_type.product_type":{"$elemMatch":type_id}},
-                                                          {"product_type.product_type.product_type":{"$elemMatch":type_id}}]})
+                        count = db["collection"].count_documents({"$or": [{"product_type": {"$elemMatch": type_id}},
+                                                                          {"product_type.product_type": {
+                                                                              "$elemMatch": type_id}},
+                                                                          {"product_type.product_type.product_type": {"$elemMatch": type_id}}]})
 
-                        print(f"INFO: Found {count} in db {db['db'].name} collection {db['collection'].name} for id   {type_id}")
-                    
+                        print(
+                            f"INFO: Found {count} in db {db['db'].name} collection {db['collection'].name} for id   {type_id}")
 
-                    #there are three levels of nesting in the product_type field
-                    works = db["collection"].find({"$or":[{"product_type":{"$elemMatch":type_id}},
-                                                          {"product_type.product_type":{"$elemMatch":type_id}},
-                                                          {"product_type.product_type.product_type":{"$elemMatch":type_id}}]})
-                    Parallel(n_jobs=self.num_jobs,backend="threading",verbose=10)(delayed(self.process_one_work)(work) for work in works)
+                    # there are three levels of nesting in the product_type field
+                    works = db["collection"].find({"$or": [{"product_type": {"$elemMatch": type_id}},
+                                                           {"product_type.product_type": {
+                                                               "$elemMatch": type_id}},
+                                                           {"product_type.product_type.product_type": {"$elemMatch": type_id}}]})
+                    Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
+                        delayed(self.process_one_work)(work) for work in works)
+
     def process_groups(self):
         """
         Method to process groups given the groups tag in the workflow configuration.
@@ -129,12 +151,19 @@ class Kahi_scienti_sample(KahiBase):
             if self.verbose > 0:
                 print("INFO: Processing groups: ", len(group_ids))
             for group_id in group_ids:
+                if "NRO_ID_GRUPO" not in group_id and "COD_ID_GRUPO" not in group_id:
+                    print(
+                        "ERROR: NRO_ID_GRUPO or COD_ID_GRUPO are required in the group configuration")
+                    raise Exception(
+                        "NRO_ID_GRUPO or COD_ID_GRUPO are required in the group configuration")
                 for db in self.dbs_in:
                     if self.verbose > 0:
-
-                        print(f"INFO: Found {db['collection'].count_documents({'group':{"$elemMatch":group_id}})} in db {db['db'].name} collection {db['collection'].name} for id   {group_id}")
-                    works = db["collection"].find({'group':{"$elemMatch":group_id}})
-                    Parallel(n_jobs=self.num_jobs,backend="threading",verbose=10)(delayed(self.process_one_work)(work) for work in works)
+                        print(
+                            f"INFO: Found {db['collection'].count_documents({'group': {"$elemMatch": group_id}})} in db {db['db'].name} collection {db['collection'].name} for id   {group_id}")
+                    works = db["collection"].find(
+                        {'group': {"$elemMatch": group_id}})
+                    Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
+                        delayed(self.process_one_work)(work) for work in works)
 
     def process_institutions(self):
         """
@@ -147,27 +176,39 @@ class Kahi_scienti_sample(KahiBase):
             if self.verbose > 0:
                 print("INFO: Processing institutions: ", len(institution_ids))
             for institution_id in institution_ids:
+                if "COD_INST" not in institution_id and ("TXT_NIT" not in institution_id and "TXT_DIGITO_VERIFICADOR" not in institution_id):
+                    print(
+                        "ERROR: COD_INST or TXT_NIT and TXT_DIGITO_VERIFICADOR are required in the institution configuration")
+                    raise Exception(
+                        "COD_INST or TXT_NIT and TXT_DIGITO_VERIFICADOR are required in the institution configuration")
+
                 for db in self.dbs_in:
                     if self.verbose > 0:
-                        print(f"INFO: Found {db['collection'].count_documents({'institution':{"$elemMatch":institution_id}})} in db {db['db'].name} collection {db['collection'].name} for id   {institution_id}")
-                    works = db["collection"].find({'institution':{"$elemMatch":institution_id}})
-                    Parallel(n_jobs=self.num_jobs,backend="threading",verbose=10)(delayed(self.process_one_work)(work) for work in works)
+                        print(
+                            f"INFO: Found {db['collection'].count_documents({'institution': {"$elemMatch": institution_id}})} in db {db['db'].name} collection {db['collection'].name} for id   {institution_id}")
+                    works = db["collection"].find(
+                        {'institution': {"$elemMatch": institution_id}})
+                    Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
+                        delayed(self.process_one_work)(work) for work in works)
+
     def process_custom_queries(self):
         """
         Method to process custom queries given the custom_queries tag in the workflow configuration.
         Custom queries are used to extract works based on custom criteria,
         this method will save in the output database all the works that match the custom query.
         """
-        if "custom_queries" in self.config["scienti_sample"] and  self.config["scienti_sample"]["custom_queries"]:
+        if "custom_queries" in self.config["scienti_sample"] and self.config["scienti_sample"]["custom_queries"]:
             queries = self.config["scienti_sample"]["custom_queries"]
             if self.verbose > 0:
                 print("INFO: Processing custom queries: ", len(queries))
             for query in queries:
                 for db in self.dbs_in:
                     if self.verbose > 0:
-                        print(f"INFO: Found {db['collection'].count_documents(query)} in db {db['db'].name} collection {db['collection'].name} for query   {query}")
+                        print(
+                            f"INFO: Found {db['collection'].count_documents(query)} in db {db['db'].name} collection {db['collection'].name} for query   {query}")
                     works = db["collection"].find(query)
-                    Parallel(n_jobs=self.num_jobs,backend="threading",verbose=10)(delayed(self.process_one_work)(work) for work in works)
+                    Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
+                        delayed(self.process_one_work)(work) for work in works)
 
     def process_custom_pipelines(self):
         """
@@ -175,7 +216,7 @@ class Kahi_scienti_sample(KahiBase):
         Custom pipelines are used to extract works based on custom criteria,
         this method will save in the output database all the works that match the custom pipeline
         """
-        if "custom_pipelines" in self.config["scienti_sample"] and  self.config["scienti_sample"]["custom_pipelines"]:
+        if "custom_pipelines" in self.config["scienti_sample"] and self.config["scienti_sample"]["custom_pipelines"]:
             pipelines = self.config["scienti_sample"]["custom_pipelines"]
             if self.verbose > 0:
                 print("INFO: Processing custom queries: ", len(pipelines))
@@ -183,9 +224,11 @@ class Kahi_scienti_sample(KahiBase):
                 for db in self.dbs_in:
                     works = list(db["collection"].aggregate(pipeline))
                     if self.verbose > 0:
-                        print(f"INFO: Found {len(works)} in db {db['db'].name} collection {db['collection'].name} for query {pipeline}")
-                    Parallel(n_jobs=self.num_jobs,backend="threading",verbose=10)(delayed(self.process_one_work)(work) for work in works)
-    
+                        print(
+                            f"INFO: Found {len(works)} in db {db['db'].name} collection {db['collection'].name} for query {pipeline}")
+                    Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
+                        delayed(self.process_one_work)(work) for work in works)
+
     def process_categories(self):
         """
         Method to process categories given the categories tag in the workflow configuration.
@@ -199,9 +242,11 @@ class Kahi_scienti_sample(KahiBase):
             for category_id in category_ids:
                 for db in self.dbs_in:
                     if self.verbose > 0:
-                        print(f"INFO: Found {db['collection'].count_documents(category_id)} in db {db['db'].name} collection {db['collection'].name} for id   {category_id}")
+                        print(
+                            f"INFO: Found {db['collection'].count_documents(category_id)} in db {db['db'].name} collection {db['collection'].name} for id   {category_id}")
                     works = db["collection"].find(category_id)
-                    Parallel(n_jobs=self.num_jobs,backend="threading",verbose=10)(delayed(self.process_one_work)(work) for work in works)
+                    Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
+                        delayed(self.process_one_work)(work) for work in works)
 
     def run(self):
         self.process_authors()
