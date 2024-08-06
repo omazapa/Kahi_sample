86001d4f6c357f4de143add0e618cdd14b746331
Omar Zapata <andresete.chaos@gmail.com>
2024-08-06 17:18:34 -0500

    [Kahi_minciencias_sample] added support to save groups and cvlac records according to the filtered products

--- Kahi_minciencias_sample/kahi_minciencias_sample/Kahi_minciencias_sample.py
+++ Kahi_minciencias_sample/kahi_minciencias_sample/Kahi_minciencias_sample.py
@@ -7,7 +7,7 @@ import re
 class Kahi_minciencias_sample(KahiBase):
     """
     Class to process the minciencias database and extract works samples based on different criteria.
-    With this class you can extract works based on authors, products, types, groups, institutions, 
+    With this class you can extract works based on authors, products, types, groups, institutions,
     categories, custom queries and custom pipelines.
 
     """
@@ -59,7 +59,8 @@ class Kahi_minciencias_sample(KahiBase):
                 if self.verbose > 0:
                     print(
                         f"INFO: Found {self.cols_in["gruplac_production"].count_documents({'id_persona_pd': author_id})} in db {self.db_in.name} collection {self.cols_in["gruplac_production"].name} for id   {author_id}")
-                works = self.cols_in["gruplac_production"].find({"id_persona_pd": author_id})
+                works = self.cols_in["gruplac_production"].find(
+                    {"id_persona_pd": author_id})
                 Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
                     delayed(self.process_one_work)(work) for work in works)
 
@@ -87,20 +88,22 @@ class Kahi_minciencias_sample(KahiBase):
                 product_ids.append(regex)
             if self.verbose > 0:
                 print("INFO: Processing products regex: ", len(product_ids))
-            total_found=0
+            total_found = 0
             for product_id in product_ids:
-                found=0
-                work_cursor = self.cols_in["gruplac_production"].find({"id_producto_pd":{"$regex": product_id}})
+                found = 0
+                work_cursor = self.cols_in["gruplac_production"].find(
+                    {"id_producto_pd": {"$regex": product_id}})
                 for work in work_cursor:
                     if self.cols_out["gruplac_production"].count_documents(work) == 0:
-                        found+=1
-                        total_found+=1
+                        found += 1
+                        total_found += 1
                         self.cols_out["gruplac_production"].insert_one(work)
                     else:
                         if self.verbose > 2:
                             print(
                                 f"INFO: Product {product_id.pattern} already exists in db {self.db_out.name} collection {self.cols_out["gruplac_production_data"].name}")
-                print(f"INFO: Found {found} works for product {product_id.pattern}")
+                print(
+                    f"INFO: Found {found} works for product {product_id.pattern}")
             print(f"INFO: Found {total_found} works in total")
 
     def process_groups(self):
@@ -115,9 +118,11 @@ class Kahi_minciencias_sample(KahiBase):
                 if self.verbose > 0:
                     print(
                         f"INFO: Found {self.cols_in["gruplac_production"].count_documents({'cod_grupo_gr': group_id})} in db {self.db_in.name} collection {self.cols_in["gruplac_production"].name} for id   {group_id}")
-                works = self.cols_in["gruplac_production"].find({"cod_grupo_gr": group_id})
+                works = self.cols_in["gruplac_production"].find(
+                    {"cod_grupo_gr": group_id})
                 Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
                     delayed(self.process_one_work)(work) for work in works)
+
     def process_categories(self):
         """
         Process categories given the COD_CATEGORY in the workflow configuration.
@@ -133,9 +138,11 @@ class Kahi_minciencias_sample(KahiBase):
                 if self.verbose > 0:
                     print(
                         f"INFO: Found {self.cols_in["gruplac_production"].count_documents({'id_tipo_pd_med': category_id})} in db {self.db_in.name} collection {self.cols_in["gruplac_production"].name} for id   {category_id.pattern}")
-                works = self.cols_in["gruplac_production"].find({"id_tipo_pd_med": category_id})
+                works = self.cols_in["gruplac_production"].find(
+                    {"id_tipo_pd_med": category_id})
                 Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
                     delayed(self.process_one_work)(work) for work in works)
+
     def process_custom_queries(self):
         """
         Process custom queries in the workflow configuration.
@@ -151,6 +158,7 @@ class Kahi_minciencias_sample(KahiBase):
                 works = self.cols_in["gruplac_production"].find(query)
                 Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
                     delayed(self.process_one_work)(work) for work in works)
+
     def process_custom_pipelines(self):
         """
         Process custom pipelines in the workflow configuration.
@@ -163,6 +171,66 @@ class Kahi_minciencias_sample(KahiBase):
                 works = self.cols_in["gruplac_production"].aggregate(pipeline)
                 Parallel(n_jobs=self.num_jobs, backend="threading", verbose=10)(
                     delayed(self.process_one_work)(work) for work in works)
+
+    def process_cvlac_stage(self):
+        """
+        Process cvlac stage in the workflow configuration.
+        """
+        person_ids = self.cols_out["gruplac_production"].distinct(
+            "id_persona_pd")
+        if self.verbose > 0:
+            print(
+                f"INFO: Processing cvlac stage, found {len(person_ids)} unique persons: ")
+        for person_id in person_ids:
+            profile = self.cols_in["cvlac_stage"].find_one(
+                {"id_persona_pr": person_id})
+            if profile:
+                if self.cols_out["cvlac_stage"].count_documents({"id_persona_pr": person_id}) == 0:
+                    self.cols_out["cvlac_stage"].insert_one(profile)
+                else:
+                    if self.verbose > 2:
+                        print(
+                            f"INFO: Profile {person_id} already exists in db {self.db_out.name} collection {self.cols_out["cvlac_stage"].name} ")
+            else:
+                print(
+                    f"INFO: Profile {person_id} not found in cvlac_stage, looking in {self.cols_out["cvlac_stage_private"].name}")
+
+                profile = self.cols_in["cvlac_stage_private"].find_one(
+                    {"id_persona_pr": person_id})
+                if profile:
+                    if self.cols_out["cvlac_stage_private"].count_documents({"id_persona_pr": person_id}) == 0:
+                        self.cols_out["cvlac_stage_private"].insert_one(
+                            profile)
+                    else:
+                        if self.verbose > 2:
+                            print(
+                                f"INFO: Profile {person_id} already exists in db {self.db_out.name} collection {self.cols_out["cvlac_stage_private"].name} ")
+                else:
+                    print(
+                        f"INFO: Profile {person_id} not found in cvlac_stage_private.")
+
+    def process_gruplac_groups(self):
+        """
+        Process gruplac groups in the workflow configuration.
+        """
+        group_ids = self.cols_out["gruplac_production"].distinct(
+            "cod_grupo_gr")
+        if self.verbose > 0:
+            print(
+                f"INFO: Processing gruplac groups, found {len(group_ids)} unique groups ")
+        for group_id in group_ids:
+            group = self.cols_in["gruplac_groups"].find_one(
+                {"cod_grupo_gr": group_id})
+            if group:
+                if self.cols_out["gruplac_groups"].count_documents({"cod_grupo_gr": group_id}) == 0:
+                    self.cols_out["gruplac_groups"].insert_one(group)
+                else:
+                    if self.verbose > 2:
+                        print(
+                            f"INFO: Group {group_id} already exists in db {self.db_out.name} collection {self.cols_out["gruplac_groups"].name} ")
+            else:
+                print(f"INFO: Group {group_id} not found in gruplac_groups.")
+
     def run(self):
         self.process_authors()
         self.process_products()
@@ -170,4 +238,6 @@ class Kahi_minciencias_sample(KahiBase):
         self.process_categories()
         self.process_custom_queries()
         self.process_custom_pipelines()
-        # return 0
+        self.process_cvlac_stage()
+        self.process_gruplac_groups()
+        return 0
